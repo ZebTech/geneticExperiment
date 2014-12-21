7e522c5fb1b466b07f65dcb0604cb481ad8eee9a
seba-1511 <seba-1511@hotmail.com>
2014-12-21 06:16:18 +0100

    More fixes and added a little deom for GA

---
+++ GA/ThreadPool/threadpool.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+class ThreadPool():
+    def __init__(self):
+        pass
+
+    def start(self):
+        pass
+
+    def join(self):
+        pass
+
+    def add(self, task):
+        return task()
