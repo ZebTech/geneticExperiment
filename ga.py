99bec1168142347e6e9fd391e91e0c5458e9f67d
seba-1511 <seba-1511@hotmail.com>
2014-12-21 03:43:42 +0100

    Implemented some of the functions in Population class

--- ga.py
+++ ga.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 
 import random
+from random import shuffle
+import copy
+import ThreadPool
 
 GOAL = 0.8
-
+INDIVIDUAL_SIZE = 10
 
 
 class GeneticAlgorithm():
@@ -16,7 +19,7 @@ class GeneticAlgorithm():
         generation = 0
         while self.predictor.predict() < GOAL:
             self.population = self.population.evolve()
-            self.optimal_individual = self.population.findOptimal(
+            self.optimal_individual = self.population.find_optimal(
                 self.predictor
             )
             print 'Generation {}, best score: {}' % (
@@ -42,4 +45,56 @@ class Individual():
 
 
 class Population():
-    pass
+    Size = 100
+    TruncateProportionalSelection = True
+    Crossover = False
+    Elitism = True
+    Tournament = True
+    SelectionStrength = 3
+    PercentElitism = 0.1
+
+    def __init__(self, individual):
+        if isinstance(individual, list):
+            self.set = individual
+        elif isinstance(individual, Individual):
+            self.set = Population.mutate(individual)
+        else:
+            self.set = Population.generate_random_population()
+
+    def find_optimal(self, pred):
+        score, optimal = -1, None
+        for p in self.set:
+            if pred.predict(p) > score:
+                optimal = p
+                score = pred.predict(p)
+        return optimal
+
+    def evolve(self, pred):
+        parents = shuffle(self.selectParents(pred))
+        new_population = []
+        if Population.Elitism:
+            nb_selected = int(Population.PercentElitism * Population.Size)
+            sorted_set = Population.sort(self.set, pred)
+            for i in xrange(nb_selected):
+                new_population.append(sorted_set[i])
+        tp = ThreadPool()
+        tp.start()
+        for i in parents:
+            father = parents.pop()
+            mother = parents.pop()
+            tp.add(generate_children(father, mother, new_population, pred))
+        tp.join()
+        self.set = new_population
+        return self
+
+    def select_parents(self, pred):
+        sorted_set = Population.sort(self.set, pred)
+        parents = []
+        if not Population.TruncateProportionalSelection:
+            return self.set
+        roulette = Population.create_roulette(sorted_set, pred)
+        for i in xrange(Population.Size):
+            parents.append(
+                roulette[GeneticAlgorithm.get_random_int(self.set.length)]
+            )
+        return parents
