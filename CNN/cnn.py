33ca128a4b5cad79b3ca98bcec4e27ccb6d33ec7
seba-1511 <seba-1511@hotmail.com>
2014-12-22 22:09:17 +0100

    Implemented LogReg (DL Tuto)

--- CNN/cnn.py
+++ CNN/cnn.py
@@ -10,6 +10,7 @@ from theano import (
 from theano.tensor.nnet import (
     conv,
     sigmoid,
+    softmax
 )
 from theano.tensor.signal import downsample
 
@@ -17,8 +18,46 @@ TRAINING_SIZE = 6000
 
 
 class LogisticRegression(object):
-    def __init__(self):
-        pass
+    def __init__(self, input, n_in, n_out):
+        self.W = self.init_weights()
+        self.b = self.init_bias()
+        self.p_y_given_x = softmax(T.dot(input, self.W) + self.b)
+        self.y_pred = T.argmax(self.p_y_given_x, axis=1)
+        self.params = [self.W, self.b]
+
+        def negative_log_likelihood(self, y):
+            return -T.mean(T.log(self.p_y_given_x)[T.arange(y.shape[0]), y])
+
+        def errors(self, y):
+            if y.ndim != self.y_pred.ndim:
+                raise TypeError(
+                    'y should have the same shape as self.y_pred',
+                    ('y', y.type, 'y_pred', self.y_pred.type)
+                )
+            if y.dtype.startswith('int'):
+                return T.mean(T.neq(self.y_pred, y))
+            else:
+                raise NotImplementedError()
+
+        def init_weights(self):
+            return shared(
+                value=np.zeros(
+                    (n_in, n_out),
+                    dtype=theano.config.floatX
+                ),
+                name='W',
+                borrow=True
+            )
+
+        def init_bias(self):
+            return shared(
+                value=np.zeros(
+                    (n_out,),
+                    dtype=theano.config.floatX
+                ),
+                name='b',
+                borrow=True
+            )
 
 
 class HiddenLayer(object):
